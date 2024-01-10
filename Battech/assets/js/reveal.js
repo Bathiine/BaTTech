6025586c83849cc9027d4dfe40fb8973f0de7011
Ali de Oliveira <alideoliveira02@gmail.com>
2024-01-10 12:17:39 -0300

    Assets BaTTech
    JavaScript e CSS do site

---
+++ Battech/assets/js/reveal.js
@@ -0,0 +1,45 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+//Topo do site
+
+revelar.reveal('.efeito-txt-topo',{
+
+    duration: 2000,
+    distance: '90px',
+})
+
+revelar.reveal('.efeito-free',{
+
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-money',{
+
+    duration: 2000,
+    distance: '90px',
+    delay: 900
+})
+revelar.reveal('.efeito-support',{
+
+    duration: 2000,
+    distance: '90px',
+    delay: 1200
+})
+
+revelar.reveal('.efeito-bg-miranha',{
+
+    duration: 1000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-ps5HA',{
+
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'right',
+})
